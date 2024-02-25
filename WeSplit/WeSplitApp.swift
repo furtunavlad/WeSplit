dedfb0bfac4f6b6ebc69e54ecc7db1f947abb97f
Vlad Furtuna <144479033+furtunavlad@users.noreply.github.com>
2024-02-25 02:15:07 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Vlad Furtuna on 25.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
